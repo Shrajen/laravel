b4f997e6aa2143dedcb84a6637280783b60300ea
Shrajen <bimeshrajbanshi1999@gmail.com>
2022-06-01 17:42:22 +0545

    Blade Practice

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/demo', function(){
+//     echo "Hi! WElcome to laravel";
+// });
+
+// Route::get('/demo1/{s}/{id?}', function ($s, $id = null){
+//     echo $s;
+//     echo $id;
+// });
+
+Route::get('/{name?}',function($name = null){
+    $data = compact('name');
+    return view('me')->with($data);
+    // echo $nam;
+});
